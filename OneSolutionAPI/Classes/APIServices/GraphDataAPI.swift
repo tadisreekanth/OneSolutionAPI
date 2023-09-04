2957acc4017adb17495a9b203a7a03539beca17f
tadisreekanth <sreekanth.t.bs@gmail.com>
2023-09-04 17:54:28 +0530

    Update GraphDataAPI.swift

--- OneSolutionAPI/Classes/APIServices/GraphDataAPI.swift
+++ OneSolutionAPI/Classes/APIServices/GraphDataAPI.swift
@@ -1,43 +1,47 @@
-////
-////  GraphDataAPI.swift
-////  OneSolutionAPI
-////
-////  Created by sreekanth reddy Tadi on 14/07/23.
-////
 //
-//import Foundation
+//  GraphDataAPI.swift
+//  OneSolutionAPI
 //
-//struct HomeAPI: AbstractAPI {
-//    static var instance: HomeAPI { HomeAPI() }
-//}
+//  Created by sreekanth reddy Tadi on 14/07/23.
 //
-//extension HomeAPI {
-//
-//    func fetchGraphData() async -> Result<GraphData, ResultError> {
-//        let params = NSMutableDictionary ()
-////        params.setValue(kUserId, forKey: "userId")
-//
-//        let endPoint = AbstractAPIEndPoint(url: ServiceAPI.shared.URL_Home_graph,
-//                                           method: .POST,
-//                                           body: params as? [String: Any])
-//        switch await self.callAPI(endPoint: endPoint) {
-//        case .success(let data):
-//            guard let data = data else {
-//                return .failure(.unknown)
-//            }
-//            if let model = data.decode(GraphData.self) {
-//                return .success(model)
-//            } else if let model = data.lowerCasedKeysData.decode(GenericModel.self) {
-//                if let errorMessage = model.message {
-//                    //show alert
-//                    return .failure(.errorMessage(errorMessage))
-//                }
-//                return .failure(.decode)
-//            }
-//            return .failure(.unknown)
-//        case .failure(let error):
-//            return .failure(error)
-//        }
-//    }
-//
-//}
+
+import Foundation
+
+struct HomeAPI: AbstractAPI {
+    static var instance: HomeAPI { HomeAPI() }
+}
+
+extension HomeAPI {
+
+    private var userId: String {
+        "\(UserData.shared.user.userID ?? 0)"
+    }
+    
+    func fetchGraphData() async -> Result<GraphData, ResultError> {
+        let params = NSMutableDictionary ()
+        params.setValue(userId, forKey: "userId")
+
+        let endPoint = AbstractAPIEndPoint(path: APIClient.shared?.path?.graphData ?? "",
+                                           method: .POST,
+                                           body: params as? [String: Any])
+        switch await self.callAPI(endPoint: endPoint) {
+        case .success(let data):
+            guard let data = data else {
+                return .failure(.unknown)
+            }
+            if let model = data.decode(GraphData.self) {
+                return .success(model)
+            } else if let model = data.lowerCasedKeysData.decode(GenericModel.self) {
+                if let errorMessage = model.message {
+                    //show alert
+                    return .failure(.errorMessage(errorMessage))
+                }
+                return .failure(.decode)
+            }
+            return .failure(.unknown)
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+
+}
